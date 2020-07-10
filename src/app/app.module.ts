25aeffc3d14d11aad504699f179b062c9c92adeb
DevBensmaine <bensmaine.hassan@gmail.com>
2020-07-10 16:15:04 +0100

    fixed bug in slide and add new Images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeSliderComponent } from './pages/home/home-slider/home-slider.component';
+import { HomeComponent } from './pages/home/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeSliderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
